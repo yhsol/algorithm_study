ce51301f33c6f4e48adef570454293de35ddb63b
loshy <loshy244110@gmail.com>
2020-07-19 17:25:11 +0900

    feat: quick sort

--- practice/rs_algorithm/src/main.rs
+++ practice/rs_algorithm/src/main.rs
@@ -1,10 +1,12 @@
-mod bubble_sort_rs;
-mod insertion_sort_rs;
-mod selection_sort_rs;
+// mod selection_sort_rs;
+// mod bubble_sort_rs;
+// mod insertion_sort_rs;
+mod quick_sort_rs;
 
 fn main() {
     // println!("Hello, world!");
     // selection_sort_rs::run();
     // bubble_sort_rs::run();
-    insertion_sort_rs::run();
+    // insertion_sort_rs::run();
+    quick_sort_rs::run();
 }
