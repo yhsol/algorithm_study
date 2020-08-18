0e7cc68f0294a3dd41fb1321fa241a9398978055
loshy <loshy244110@gmail.com>
2020-08-18 21:27:08 +0900

    feat: sorting condition

---
+++ code_test/acmicpc_11650_2.py
@@ -0,0 +1,10 @@
+N = int(input())
+a = []
+for _ in range(N):
+    a.append(list(map(int, input().split())))
+
+# 정렬 조건: 첫번째 인자를 먼저 정렬하고, 그 안에서 두번째 인자에 따라 정렬
+a.sort(key=lambda data: (data[0], data[1]))
+
+for [i, j] in a:
+    print("result: ", i, j)
