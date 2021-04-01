5080630cf560ce76a92de97157319a5be05b9241
yhsol <loshY244110@gmail.com>
2021-04-01 18:35:48 +0900

    feat: 폴더 구조 정리 및 파이썬 알고리즘 인터뷰

---
+++ book/python_algorithm_interview/chapter3.py
@@ -0,0 +1,48 @@
+def lambda_fn():
+    return list(map(lambda x: x + 10, [1, 2, 3]))
+
+
+def list_comprehension_fn():
+    return [n * 2 for n in range(1, 10 + 1) if n % 2 == 1]
+
+
+def normal_fn():
+    a = []
+    for n in range(1, 10+1):
+        if n % 2 == 1:
+            a.append(n * 2)
+    return a
+
+
+def get_natural_number():
+    n = 0
+    while True:
+        n += 1
+        yield n
+
+
+def print_100_nums():
+    g = get_natural_number()
+    for _ in range(0, 100):
+        print(next(g))
+
+
+def generator():
+    yield 1
+    yield 'string'
+    yield True
+
+
+# print('run: ', print_100_nums())
+
+g = generator()
+print(g)
+print(next(g))
+print(next(g))
+print(next(g))
+
+print(list(range(5)))
+print(range(5))
+print(type(range(5)))
+for i in range(5):
+    print(i, end=' ')
