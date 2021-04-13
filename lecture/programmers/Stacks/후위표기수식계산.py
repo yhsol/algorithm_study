2b5316a7d64e8fb080684e6b3ba71e4bdab5b2d3
yhsol <loshY244110@gmail.com>
2021-04-13 21:42:56 +0900

    feat: 13강: 후위 표기 수식 계산

---
+++ lecture/programmers/Stacks/후위표기수식계산.py
@@ -0,0 +1,67 @@
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+
+def splitTokens(exprStr):
+    tokens = []
+    val = 0
+    valProcessing = False
+    for c in exprStr:
+        if c == ' ':
+            continue
+        if c in '0123456789':
+            val = val * 10 + int(c)
+            valProcessing = True
+        else:
+            if valProcessing:
+                tokens.append(val)
+                val = 0
+            valProcessing = False
+            tokens.append(c)
+    if valProcessing:
+        tokens.append(val)
+
+    return tokens
+
+
+def infixToPostfix(tokenList):
+    prec = {
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+        '(': 1,
+    }
+
+    opStack = ArrayStack()
+    postfixList = []
+
+    return postfixList
+
+
+def postfixEval(tokenList):
+    pass
+
+
+def solution(expr):
+    tokens = splitTokens(expr)
+    postfix = infixToPostfix(tokens)
+    val = postfixEval(postfix)
+    return val
