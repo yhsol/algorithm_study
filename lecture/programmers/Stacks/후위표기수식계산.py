b083a9a4cb1d7381139b15e8a3a22d6525ecdbae
yhsol <loshY244110@gmail.com>
2021-04-16 20:59:14 +0900

    feat: 후위표기수식계산

--- lecture/programmers/Stacks/후위표기수식계산.py
+++ lecture/programmers/Stacks/후위표기수식계산.py
@@ -1,7 +1,3 @@
-# stacks
-from stacks import ArrayStack as Stack
-
-
 class ArrayStack:
 
     def __init__(self):
@@ -55,39 +51,84 @@ def infixToPostfix(tokenList):
     }
 
     opStack = ArrayStack()
+    prec_list = ["*", "/", "+", "-", "(", ")"]
+
+    answer = ''
+
     postfixList = []
 
-    for token in tokenList:
-        if type(token) is int:
-            pass
-        elif token == "(":
-            pass
-        elif token == ")":
-            pass
+    for i in tokenList:
+        if i not in prec_list:  # 피연산자
+            # answer += i
+            postfixList.append(i)
         else:
-            pass
+            if opStack.isEmpty():  # stack 이 비었을 때
+                opStack.push(i)
+            elif i == "(":
+                opStack.push(i)
+            elif "(" in opStack.data:
+                if i != ")":
+                    opStack.push(i)
+                elif i == ")":
+                    reversed_Stack = reversed(opStack.data)
+                    for j in reversed_Stack:
+                        if j != "(":
+                            # answer += opStack.pop()
+                            postfixList.append(opStack.pop())
+                        else:
+                            opStack.pop()
+                            break
+            else:   # 연산자
+                peeked = opStack.peek()
+                if prec[i] <= prec[peeked]:
+                    # [v] 비교하려는 연산자가 스택에 있는 연산자보다 작을 때,
+                    # 한번 만 비교하는 게 아니라 그 다음 연산자와도 계속 비교해야함.
+                    # 기존에는 한번만 비교를 해서 실패
+                    # while 조건을 추가해서 통과
+                    while prec[i] <= prec[peeked] and not opStack.isEmpty():
+                        # answer += opStack.pop()
+                        postfixList.append(opStack.pop())
+                    opStack.push(i)
+                else:
+                    prec[i] > prec[peeked]
+                    opStack.push(i)
 
     while not opStack.isEmpty():
-        pass
+        for i in opStack.data:
+            # answer += opStack.pop()
+            postfixList.append(opStack.pop())
 
     return postfixList
 
 
+def calc(valStack):
+    second = valStack.pop()
+    first = valStack.pop()
+    return first, second
+
+
 def postfixEval(tokenList):
-    valStack = Stack()
+    valStack = ArrayStack()
 
+    print(tokenList)
+    # [1, 2, '+', 3, 4, '+', '*']
     for token in tokenList:
-        if type(token) is int:
-            pass
-        elif token == "*":
-            pass
-        elif token == "/":
-            pass
-        elif token == "+":
-            pass
-        elif token == "-":
-            pass
+        print(valStack.data)
 
+        if type(token) is int:
+            valStack.push(token)
+        elif token == "*":
+            first, second = calc(valStack)
+            valStack.push(first * second)
+        elif token == "/":
+            first, second = calc(valStack)
+            valStack.push(first / second)
+        elif token == "+":
+            first, second = calc(valStack)
+            valStack.push(first + second)
+        elif token == "-":
+            first, second = calc(valStack)
+            valStack.push(first - second)
     return valStack.pop()
 
 
@@ -96,3 +137,6 @@ def solution(expr):
     postfix = infixToPostfix(tokens)
     val = postfixEval(postfix)
     return val
+
+
+print(solution('(1 + 2) * (3 + 4)'))
