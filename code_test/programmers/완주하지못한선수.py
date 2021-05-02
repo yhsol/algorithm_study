a9190183fd4d234805b68419a2ada8349e6468d0
yhsol <loshY244110@gmail.com>
2021-05-02 16:12:12 +0900

    feat: 완주하지못한선수

--- code_test/programmers/완주하지못한선수.py
+++ code_test/programmers/완주하지못한선수.py
@@ -1,8 +1,46 @@
 def solution(participant, completion):
     answer = ''
+    part_dict = {}
+    comp_dict = {}
+    for part in participant:
+        if part in part_dict:
+            part_dict[part] += 1
+        else:
+            part_dict[part] = 0
+
+    for comp in completion:
+        if comp in comp_dict:
+            comp_dict[comp] += 1
+        else:
+            comp_dict[comp] = 0
+
     for i in participant:
-        if i in completion:
-            completion.remove(i)
-        elif i not in completion:
-            answer = i
-    return answer
+        if i not in comp_dict:
+            return i
+        elif part_dict[i] != comp_dict[i]:
+            return i
+
+
+def solution2(participant, completion):
+    answer = ''
+    part_dict = {}
+    for part in participant:
+        if part in part_dict:
+            part_dict[part] += 1
+        else:
+            part_dict[part] = 1
+
+    for comp in completion:
+        if comp not in part_dict:
+            return comp
+        else:
+            part_dict[comp] -= 1
+
+    for i in part_dict:
+        if part_dict[i] != 0:
+            return i
+
+
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+print(solution(participant, completion))
