8f87581481e1aab088ee7603f7a239b8d6e2f825
yhsol <loshY244110@gmail.com>
2021-05-12 17:50:35 +0900

    feat: chapter11

---
+++ book/python_algorithm_interview/chapter11.py
@@ -0,0 +1,17 @@
+import random
+
+
+def birthdayProblem():
+    TRIALS = 100000
+    same_birthdays = 0
+
+    for _ in range(TRIALS):
+        birthdays = []
+        for i in range(23):
+            birthday = random.randint(1, 365)
+            if birthday in birthdays:
+                same_birthdays += 1
+                break
+            birthdays.append(birthday)
+
+    print(f'{same_birthdays / TRIALS * 100}%')
