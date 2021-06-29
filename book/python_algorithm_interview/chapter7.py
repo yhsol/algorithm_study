b4d9bcfd21629e17988c5a0e15a6e5d40e60fdfd
yhsol <loshY244110@gmail.com>
2021-06-29 19:36:28 +0900

    feat: twoSum

--- book/python_algorithm_interview/chapter7.py
+++ book/python_algorithm_interview/chapter7.py
@@ -310,4 +310,19 @@ def maxProf(prices: List[int]) -> int:
     return profit
 
 
-print(stock([7, 1, 5, 3, 6, 4]))
+def two_sum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums) - 1):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+def two_sum_2(nums: List[int], target: int) -> List[int]:
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i+1:]:
+            return [i, nums[i+1:].index(complement) + (i + 1)]
+
+
+print(two_sum_2([2, 7, 11, 15], 9))
