ce71fe49dea8e96402a579affe8bd79c2289ad52
yhsol <loshY244110@gmail.com>
2021-07-05 20:16:44 +0900

    feat: 3sum

--- book/python_algorithm_interview/problems/09-3sum.py
+++ book/python_algorithm_interview/problems/09-3sum.py
@@ -14,14 +14,12 @@ def threeSum_1(nums: List[int]) -> List[List[int]]:
                     sorted_sum = sorted([nums[i], nums[j], nums[k]])
                     if sorted_sum not in result:
                         result.append(sorted_sum)
-
     return result
 
 
 def threeSum_2(nums: List[int]) -> List[List[int]]:
     results = []
     nums.sort()
-
     for i in range(len(nums) - 2):
         if i > 0 and nums[i] == nums[i-1]:
             continue
@@ -40,7 +38,6 @@ def threeSum_2(nums: List[int]) -> List[List[int]]:
 def threeSum_3(nums: List[int]) -> List[List[int]]:
     results = []
     nums.sort()
-
     for i in range(len(nums) - 2):
         if i > 0 and nums[i] == nums[i-1]:
             continue
@@ -57,6 +54,60 @@ def threeSum_3(nums: List[int]) -> List[List[int]]:
                 while left < right and nums[left] == nums[left + 1]:
                     left += 1
                 while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return results
+
+
+def threeSum_4(nums: List[int]) -> List[List[int]]:
+    results = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+                results.append((nums[i], nums[left], nums[right]))
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+                while left < right and nums[right] == nums[right-1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return results
+
+
+def threeSum_5(nums: List[int]) -> List[List[int]]:
+    results = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+                results.append([nums[i]], nums[left], nums[right])
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+                while left < right and nums[right] == nums[right-1]:
                     right -= 1
                 left += 1
                 right -= 1
