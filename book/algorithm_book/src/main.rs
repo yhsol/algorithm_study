6c293422c4df892b7a20dd7d2fc7e8c1746480c0
yhsol <loshY244110@gmail.com>
2021-09-06 21:16:38 +0900

    feat: unit 05

--- book/algorithm_book/src/main.rs
+++ book/algorithm_book/src/main.rs
@@ -1,7 +1,8 @@
 mod unit_03;
 mod unit_04;
+mod unit_05;
 
 fn main() {
     println!("here is main.rs");
-    println!("result is here: {:?}", unit_04::run())
+    println!("result is here: {:?}", unit_05::run())
 }
--- book/algorithm_book/src/unit_05.rs
+++ book/algorithm_book/src/unit_05.rs
@@ -1,3 +1,22 @@
-pub fn run() {}
+pub fn run() {
+    println!("n = 1");
+    hanoi(1, 1, 3, 2);
+    println!("n = 2");
+    hanoi(2, 1, 3, 2);
+    println!("n = 3");
+    hanoi(3, 1, 3, 2);
+}
 
-pub fn hanoi() {}
+pub fn hanoi(n: i32, from: i32, to: i32, aux: i32) {
+    // 종료 조건
+    // 원반 한 개를 옮기는 문제면 그냥 옮기면 됨
+    if n == 1 {
+        println!("{} -> {}", from, to);
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);
+    println!("{} -> {}", from, to);
+
+    hanoi(n - 1, aux, to, from);
+}
