e124d7644c0c40673b48f044b1d0fb9f16eb0aa4
yhsol <loshY244110@gmail.com>
2021-10-09 16:24:28 +0900

    feat: Palindrome Linked List with Deque

--- book/python_algorithm_interview/Q/13.py
+++ book/python_algorithm_interview/Q/13.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from typing import Deque, Optional
+import collections
 
 # Definition for singly-linked list.
 
@@ -25,4 +26,18 @@ class Solution:
                 return False
             left += 1
             right -= 1
+        return True
+
+    def isPalindromeDeque(self, head: Optional[ListNode]) -> bool:
+        listed: Deque = collections.deque()
+
+        node = head
+
+        while node != None:
+            listed.append(node.val)
+            node = node.next
+
+        while len(listed) > 1:
+            if listed.popleft() != listed.pop():
+                return False
         return True
