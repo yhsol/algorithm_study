4b73879906ad3932bc7d9f80b1d28f151c21ed42
yhsol <loshY244110@gmail.com>
2021-11-09 21:16:46 +0900

    feat: Q21

--- book/python_algorithm_interview/Q/21.py
+++ book/python_algorithm_interview/Q/21.py
@@ -1,3 +1,6 @@
+import collections
+
+
 def no_duplicated(s: str) -> str:
     formatted = []
     for item in s:
@@ -6,3 +9,23 @@ def no_duplicated(s: str) -> str:
         else:
             formatted.append(item)
     return sorted(formatted)
+
+
+def no_duplicated_stack(s: str) -> str:
+    counter, seen, stack = collections.Counter(s), set(), []
+
+    for char in s:
+        counter[char] -= 1
+        if char in seen:
+            continue
+
+        # stack 이 남아있고,
+        # stack 의 마지막 값 보다 char 가 작고,
+        # stakc 의 마지막 값이 뒤에 남아있을 때
+        while stack and char < stack[-1] and counter[stack[-1]] > 0:
+            seen.remove(stack.pop())
+
+        stack.append(char)
+        seen.add(char)
+
+    return ''.join(stack)
