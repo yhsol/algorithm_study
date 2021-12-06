13900fcacc1adc2869d5dc84421be897fb5a48e8
yhsol <loshY244110@gmail.com>
2021-12-06 15:53:32 +0900

    feat: 오픈채팅방

---
+++ code_test/programmers/오픈채팅방.ts
@@ -0,0 +1,49 @@
+const STATUS = {
+  Leave: "Leave",
+  Change: "Change",
+  Enter: "Enter",
+};
+
+const ACTION_BY_STATUS = {
+  Leave: "님이 나갔습니다.",
+  Enter: "님이 들어왔습니다.",
+};
+
+function solutionFn(record: string[]): string[] {
+  let answer = [];
+  let userDict = {};
+
+  for (let item of record) {
+    let splited = item.split(" ");
+    let status = splited[0];
+
+    if (status !== STATUS.Leave) {
+      let uid = splited[1];
+      let name = splited.length > 2 ? splited[2] : "";
+
+      userDict[uid] = {
+        name,
+      };
+    }
+  }
+
+  for (let item of record) {
+    let splited = item.split(" ");
+    let status = splited[0];
+    let uid = splited[1];
+
+    if (status !== STATUS.Change) {
+      answer.push(`${userDict[uid].name}${ACTION_BY_STATUS[status]}`);
+    }
+  }
+
+  return answer;
+}
+
+const record = [
+  "Enter uid1234 Muzi",
+  "Enter uid4567 Prodo",
+  "Leave uid1234",
+  "Enter uid1234 Prodo",
+  "Change uid4567 Ryan",
+];
