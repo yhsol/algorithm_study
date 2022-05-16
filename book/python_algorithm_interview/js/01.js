54c21d64bbdd375026eb1cc0c6ad3e3caf4b1896
Yang HanSol <43926614+yhsol@users.noreply.github.com>
2022-05-16 12:44:23 +0000

    feat: 01

---
+++ book/python_algorithm_interview/js/01.js
@@ -0,0 +1,19 @@
+function solution(s) {
+  let filtered = s.split('').filter(item => /^[a-zA-Z0-9]+$/.test(item)).map(item => item.toLowerCase())
+  
+  // console.log('origin: ', filtered.join(''))
+  // console.log('reversed: ', filtered.reverse().join(''))
+  // while (filtered.length > 1) {
+  //   const front = filtered.shift()
+  //   const rear = filtered.pop();
+  //   console.log('here: ',front, rear)
+  //   if (front !== rear) {
+  //     return false
+  //   }
+  // }
+  // return true
+
+  return filtered.join('') === filtered.reverse().join('')
+}
+
+console.log(solution("A man, a plan, a canal: Panama"))
