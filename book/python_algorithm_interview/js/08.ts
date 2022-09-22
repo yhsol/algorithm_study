b0fb9d2eb8c42271a57da089bee8aeaeeae51593
yhsol <loshy244110@gmail.com>
2022-09-22 20:46:11 +0900

    feat: trapping-rain-water - py, ts

--- book/python_algorithm_interview/js/08.ts
+++ book/python_algorithm_interview/js/08.ts
@@ -1,9 +1,38 @@
-const solution = (data: number[]) => {
-  const big = Math.max(...data);
-  const pivotIndex = data.indexOf(big);
-  console.log(pivotIndex);
-};
+// algorithm: Math.min(maxL, maxR) - currentHeight
 
-console.log(solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+class Solution {
+  constructor() {}
 
-export {};
+  trap(height: number[]): number {
+    if (!height.length) return 0;
+
+    let result = 0;
+    let left = 0;
+    let maxL = height[left];
+    let right = height.length - 1;
+    let maxR = height[right];
+
+    while (left < right) {
+      let value = 0;
+      if (maxL < maxR) {
+        left += 1;
+        const curr = height[left];
+        value = maxL - curr;
+        maxL = Math.max(maxL, curr);
+      } else {
+        right -= 1;
+        const curr = height[right];
+        value = maxR - curr;
+        maxR = Math.max(maxR, curr);
+      }
+
+      if (value > 0) {
+        result += value;
+      }
+    }
+
+    return result;
+  }
+}
+
+console.log(new Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
