808a2c23b37aa76b210c61545a0e494d192141eb
yhsol <loshy244110@gmail.com>
2022-12-21 19:56:20 +0900

    feat: leetcode 234

--- Q/13.ts
+++ Q/13.ts
@@ -11,17 +11,19 @@ export class ListNode {
   }
 }
 
-function isPalindrome(head: ListNode | null): boolean {
+function isPalindromeByIterative(head: ListNode | null): boolean {
   let q: number[] = [];
 
   if (!head) return true;
 
-  let node: ListNode | null = head;
+  let currNode: ListNode | null = node;
 
-  while (!!node) {
-    q.push(node.val);
-    node = node.next;
+  while (!!currNode) {
+    q.push(currNode.val);
+    currNode = currNode.next;
   }
+
+  console.log("q : isPalindromeByIterative: ", q);
 
   while (q.length > 1) {
     if (q.shift() !== q.pop()) {
@@ -31,3 +33,33 @@ function isPalindrome(head: ListNode | null): boolean {
 
   return true;
 }
+
+function isPalindromeByRecursive(head: ListNode | null): boolean {
+  let q: number[] = [];
+
+  if (!head) return true;
+
+  const makeArray = (node: ListNode) => {
+    q.push(node.val);
+
+    if (node.next) {
+      makeArray(node.next);
+    }
+  };
+
+  makeArray(head);
+
+  console.log("q : isPalindromeByRecursive: ", q);
+
+  while (q.length > 1) {
+    if (q.shift() !== q.pop()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let node = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+console.log(isPalindromeByIterative(node));
+console.log(isPalindromeByRecursive(node));
