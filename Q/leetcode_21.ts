1bfbbc42caf3e3082d19c1db52e181bd930f0639
yhsol <loshy244110@gmail.com>
2022-12-29 16:04:06 +0900

    feat: leetcode 21

--- Q/leetcode_21.ts
+++ Q/leetcode_21.ts
@@ -1,3 +1,16 @@
+/**
+ * Merget Tow Sorted List
+ *
+ * [1,3,5] list, [1,2,3,4] list.
+ * 두 리스트는 정렬되어 있는 상태.
+ * 이걸 합쳐서 [1,1,2,3,3,4,5] 와 같이 정렬된 리스트로 만들기.
+ *
+ * 첫번째 리스트의 head를 가리키는 node1, 두번째 리스트의 head를 가리키는 node2 를 만든다.
+ * 그래서 둘을 비교하고 작은쪽을 output list 에 담는다.
+ * 담은쪽 노드의 포인터를 그 다음으로 옮긴다.
+ * output 노드를 만들기 위해서 dummy 노드 생성.
+ */
+
 class ListNode {
   val: number;
   next: ListNode | null;
@@ -5,6 +18,50 @@ class ListNode {
     this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
+}
+
+function mergeTwoLists2(list1: ListNode | null, list2: ListNode | null) {
+  let dummy = new ListNode();
+  let [node1, node2] = [list1, list2];
+  let curr = dummy;
+
+  // node1 과 node2 를 비교.
+  // node1 이 더 작으면 output 과 연결된 curr 의 next 를 node1 으로 연결.
+  // node1 은 이제 통과이기때문에 next 로 이동. node1 = node1.next;
+  // curr 노드는 최신의 상태로 업데이트 해줘야함. curr = curr.next; 로 이동.
+  while (node1 && node2) {
+    if (node1.val < node2.val) {
+      curr.next = node1;
+      node1 = node1.next;
+      curr = curr.next;
+    } else {
+      curr.next = node2;
+      node2 = node2.next;
+      curr = curr.next;
+    }
+  }
+
+  // 남은 노드들은 그대로 다 붙이기.
+  if (node1) {
+    curr.next = node1;
+  }
+
+  if (node2) {
+    curr.next = node2;
+  }
+
+  // while (node1) {
+  //   curr.next = node1;
+  //   node1 = node1.next;
+  //   curr = curr.next;
+  // }
+  // while (node2) {
+  //   curr.next = node2;
+  //   node2 = node2.next;
+  //   curr = curr.next;
+  // }
+
+  return dummy.next;
 }
 
 function mergeTwoLists(
