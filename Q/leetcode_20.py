ca9caeb48bae366c5bffbd8df5aab3d6427e404c
yhsol <loshy244110@gmail.com>
2023-04-08 01:54:46 +0900

    feat: study with chatGPT by leetcode 20

--- Q/leetcode_20.py
+++ Q/leetcode_20.py
@@ -23,5 +23,47 @@ class Solution:
             return False
 
         return True
-    
-solution = Solution().isValid('(){}[]')
+
+    def isValidByChatGPT(self, s: str) -> bool:
+
+        # 1. 여는 괄호를 닫는 괄호에 매핑하는 사전을 정의
+        dict = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        stack = []
+
+        for char in s:
+            # opening brackets
+            if char in dict.values():
+                stack.append(char)
+            # closing brackets
+            elif char in dict.keys():
+                if not stack or stack.pop() != dict[char]:
+                    return False
+            # not opening brackets and not closing brackets
+            # 그치 value 도 아니고 key 도 아니면 False 지.
+            else:
+                return False
+
+        return not stack  # 똑똑하당. 이렇게 체크해서 리턴해버리면 되는 군.
+
+    def isValidBetterPerformanceWithChatGPT(self, s: str):
+        opening_brackets = '([{'
+        closing_brackets = ')]}'
+
+        stack = []
+
+        for char in s:
+            if char in opening_brackets:
+                stack.append(char)
+            else:
+                if len(stack) == 0 or closing_brackets.index(char) != opening_brackets.index(stack.pop()):
+                    return False
+
+        return len(stack) == 0
+
+
+solution = Solution().isValidBetterPerformanceByChatGPT('(){}[]')
