269806c9a48052b6b757eec64399ea350b8f32e1
yhsol <loshy244110@gmail.com>
2023-07-31 10:36:16 +0900

    feat: leetcode 622

--- Q/leetcode_622.ts
+++ Q/leetcode_622.ts
@@ -1,43 +1,67 @@
 class MyCircularQueue {
-  private queue: number[] = [];
+  private queue: (number | null)[] = [];
   private size: number;
+  private p1: number = 0;
+  private p2: number = 0;
 
   constructor(k: number) {
+    this.queue = new Array(k).fill(null);
     this.size = k;
   }
 
   enQueue(value: number): boolean {
-    if (this.isFull()) {
+    if (this.queue[this.p2] === null) {
+      this.queue[this.p2] = value;
+      this.p2 = (this.p2 + 1) % this.size;
+      return true;
+    } else {
       return false;
     }
 
-    this.queue.push(value);
-    return true;
+    // if (this.isFull()) {
+    //   return false;
+    // }
+
+    // this.queue.push(value);
+    // return true;
   }
 
   deQueue(): boolean {
-    if (this.isEmpty()) {
+    if (this.queue[this.p1] === null) {
       return false;
+    } else {
+      this.queue[this.p1] = null;
+      this.p1 = (this.p1 + 1) % this.size;
+      return true;
     }
 
-    this.queue.shift();
-    return true;
+    // if (this.isEmpty()) {
+    //   return false;
+    // }
+
+    // this.queue.shift();
+    // return true;
   }
 
   Front(): number {
-    return this.queue[0] ?? -1;
+    return this.queue[this.p1] === null ? -1 : this.queue[this.p1]!;
+    // return this.queue[0] ?? -1;
   }
 
   Rear(): number {
-    return this.queue[this.queue.length - 1] ?? -1;
+    const last = this.p2 <= 0 ? this.size - 1 : this.p2 - 1;
+    return this.queue[last] === null ? -1 : this.queue[last]!;
+    // return this.queue[this.queue.length - 1] ?? -1;
   }
 
   isEmpty(): boolean {
-    return this.queue.length === 0;
+    return this.p1 === this.p2 && this.queue[this.p1] === null;
+    // return this.queue.length === 0;
   }
 
   isFull(): boolean {
-    return this.queue.length === this.size;
+    return this.p1 === this.p2 && this.queue[this.p1] !== null;
+    // return this.queue.length === this.size;
   }
 }
 
