6ad4bdcccd1c1a726a96498bb3736f0a0bcc248b
yhsol <loshy244110@gmail.com>
2024-10-20 20:59:41 +0900

    feat: leetcode 344
    - reverse 로도 잘 풀림
    - 투포인터는 역시 쓸데가 많다.
    - swap 하는 방식을 잊고 있었는데 solutions 보고 오랜만에 다시 봄.

--- Q/Q2.ts
+++ Q/Q2.ts
@@ -7,17 +7,32 @@
 const s = ["h", "e", "l", "l", "o"];
 
 function reverseString(s: string[]): void {
+  s.reverse();
+}
+
+function reverseString2(s: string[]): void {
   let left = 0;
   let right = s.length - 1;
 
   while (left < right) {
-    const l = s[left];
-    const r = s[right];
-    s[left] = r;
-    s[right] = l;
+    let copiedLeft = s[left];
+    let copiedRight = s[right];
+    s[left] = copiedRight;
+    s[right] = copiedLeft;
     left++;
     right--;
   }
 }
 
-console.log(reverseString(s));
+function reverseString3(s: string[]): void {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+}
+
+console.log(reverseString2(s));
